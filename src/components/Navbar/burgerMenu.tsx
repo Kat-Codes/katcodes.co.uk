3b530c8b40adbbf14914f88b44fda807d01997d3
Katie <katiew812@hotmail.co.uk>
2021-04-04 13:06:11 +0100

    :sparkles: Add animation to burger menu

--- src/components/Navbar/burgerMenu.tsx
+++ src/components/Navbar/burgerMenu.tsx
@@ -1,21 +1,48 @@
 import React from 'react';
 import styled from 'styled-components';
+import gsap from 'gsap';
 
-const BurgerMenu = ({navbarState, handleNavbar } : MenuProps) => {
+const commonAnim = {
+    duration: 0.1,
+};
+
+const BurgerMenu = ({ navbarState, handleNavbar }: MenuProps) => {
+    const tl = gsap.timeline();
+
     const sendClickEvent = () => {
         handleNavbar();
+        updateMenu();
     };
-    
+
+    const updateMenu = () => {
+        if (navbarState) {
+            tl.to('.one', { ...commonAnim, rotation: 45, y: 14 })
+                .to('.two', { ...commonAnim, width: 0, x: 25 }, 0)
+                .to('.three', { ...commonAnim, rotation: -45, y: -14 }, 0);
+        } else {
+            tl.to('.one', { ...commonAnim, rotation: 0, y: 0 })
+                .to('.two', { ...commonAnim, width: 50, x: 0 }, 0)
+                .to('.three', { ...commonAnim, rotation: 0, y: 0 }, 0);
+        }
+    };
+
+    updateMenu();
+
     return (
         <Wrapper onClick={sendClickEvent}>
             <FlexWrapper className={navbarState ? 'open' : ''}>
-                <span>&nbsp;</span>
-                <span>&nbsp;</span>
-                <span>&nbsp;</span>
+                <span className='one'>&nbsp;</span>
+                <span className='two'>&nbsp;</span>
+                <span className='three'>&nbsp;</span>
             </FlexWrapper>
         </Wrapper>
     );
 };
+
+var tl = gsap.timeline();
+tl.to('.box1', { duration: 2, x: 100 })
+    .to('.box2', { duration: 1, y: 200 })
+    .to('.box3', { duration: 3, rotation: 360 });
 
 type MenuProps = {
     navbarState: boolean;
@@ -43,21 +70,6 @@ const Wrapper = styled.div`
         transform: rotate(0deg);
         transition: all ease-in-out 0.2s;
     }
-
-    /* .open span:nth-child(3) {
-        transform: rotate(45deg);
-        top: -14px;
-    }
-
-    .open span:nth-child(2) {
-        width: 0;
-    }
-
-    .open span:nth-child(1) {
-        transform: rotate(-45deg);
-        top: 14px;
-        left: 2px;
-    } */
 `;
 
 export default BurgerMenu;
