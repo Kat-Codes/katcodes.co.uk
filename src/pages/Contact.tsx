f5eef59398bdcb7a057ba3f2bbf826675c7b57cf
Katie Walker <katiew812@hotmail.co.uk>
2021-05-01 14:14:41 +0100

    Comment out import

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled from 'styled-components';
+// import styled from 'styled-components';
 import gsap from 'gsap';
 import ContactForm from '../components/Form';
 
